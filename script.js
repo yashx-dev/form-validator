0b470afe273625ea56be374b2e1e0cd983bd0f71
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2025-12-22 06:40:11 +0530

    Submit button compelted with fake data sending simulation

--- script.js
+++ script.js
@@ -19,12 +19,6 @@
 // termsError
 // phoneError
 
-
-
-
-
-// there's a bug to solve that bug you just need to remove event listner from registrationform and remove all other function and little dedbugging in email because of space's preventDefault()
-
 const fullName = document.getElementById("fullName");
 const nameError = document.getElementById("nameError");
 const email = document.getElementById("email");
@@ -46,7 +40,7 @@ let termsError = document.getElementById("termsError");
 const usernameError = document.getElementById("usernameError");
 const registrationForm = document.getElementById("registrationForm");
 let tag = document.getElementsByClassName("errortag");
-
+const blankcheck = document.getElementsByClassName("blank-check");
 let UserData = {
   Name: "",
   EmailAdress: "",
@@ -74,206 +68,221 @@ function passwordcheck3(value) {
   return /[!@#$%^&*_-]/.test(value);
 }
 
-registrationForm.addEventListener("click", (c) => {
-  if (c.target.id === "fullName") {
-    // console.log(c.target.id);
-    Namecheck();
-  } else if (c.target.id === "email") {
-    emailcheck();
-  } else if (c.target.id === "phone") {
-    phonecheck();
-  } else if (c.target.id === "username") {
-    usercheck();
-  } else if (c.target.id === "passwordInput") {
-    passwordchecker();
-  } else if (c.target.id === "confirmPassword") {
-    confirm();
-  } else if (c.target.id === "terms") {
-    checkbox();
+fullName.addEventListener("change", (e) => {
+  Name = e.target.value;
+  // console.log(Name);
+  Name = Name.trim();
+  if (Name.length < 2) {
+    nameError.classList.remove("hidden");
+    nameError.innerHTML = "Name should have atleast 2 Characters";
+  } else {
+    UserData.Name = Name;
+    nameError.classList.add("hidden");
   }
+  console.log(UserData);
 });
 
-function Namecheck() {
-  fullName.addEventListener("change", (e) => {
-    Name = e.target.value;
-    // console.log(Name);
-    Name.trim();
-    if (Name.length < 2) {
-      nameError.classList.remove("hidden");
-      nameError.innerHTML = "Name should have atleast 2 Characters";
-    } else {
-      UserData.Name = Name;
-      nameError.classList.add("hidden");
-    }
-    console.log(UserData);
+email.addEventListener("input", (e) => {
+  Email = e.target.value;
+  if (Email.includes(" ")) {
+    Email = Email.replace(/\s/g, "");
+    e.target.value = Email;
+  }
+  Email = Email.toLowerCase();
+  if (!emailregex(Email)) {
+    emailError.classList.remove("hidden");
+    emailError.innerHTML = "Invalid Email";
+  } else {
+    UserData.EmailAdress = Email;
+    emailError.classList.add("hidden");
+  }
+  console.log(UserData);
+});
+
+phone.addEventListener("change", (e) => {
+  Phone = e.target.value;
+  if (!number(Phone)) {
+    phoneError.classList.remove("hidden");
+    phoneError.innerHTML = "Invalid Mobile Number";
+  } else {
+    // Phone.replace(/(\d{5})/ , '$1 ')
+    UserData.PhoneNumber = Phone;
+    phoneError.classList.add("hidden");
+  }
+  console.log(UserData);
+});
+
+username.addEventListener("change", (e) => {
+  User = e.target.value;
+  if (!userregex(User)) {
+    usernameError.classList.remove("hidden");
+    usernameError.innerHTML =
+      "Invalid Username Maximum 15 Characters and only (Uppercase, Lowercase, Number)";
+  } else {
+    UserData.UserName = User;
+    usernameError.classList.add("hidden");
+  }
+  console.log(UserData);
+});
+
+toggle.addEventListener("click", () => {
+  input.type === "password" ? (input.type = "text") : (input.type = "password");
+});
+
+input.addEventListener("input", (e) => {
+  target = e.target.value;
+  console.log(target);
+  if (target.length < 8) {
+    passwordError.classList.remove("hidden");
+    passwordError.innerHTML = "Password must be greater than 8";
+    passwordStrength.classList.remove(
+      "bg-green-400",
+      "bg-orange-400",
+      "bg-gray-700"
+    );
+    passwordStrength.classList.add("bg-red-400");
+  } else if (
+    passwordcheck(target) &&
+    passwordcheck2(target) &&
+    passwordcheck3(target)
+  ) {
+    passwordStrengthText.innerHTML = "Strong";
+    passwordStrength.classList.remove(
+      "bg-red-400",
+      "bg-orange-400",
+      "bg-gray-700"
+    );
+    passwordStrength.classList.add("bg-green-400");
+    passwordError.classList.add("hidden");
+  } else if (
+    (passwordcheck(target) && passwordcheck2(target)) ||
+    (passwordcheck2(target) && passwordcheck3(target)) ||
+    (passwordcheck(target) && passwordcheck3(target))
+  ) {
+    passwordStrength.classList.add("bg-orange-400");
+    passwordStrength.classList.remove(
+      "bg-green-400",
+      "bg-red-400",
+      "bg-gray-700"
+    );
+    passwordStrengthText.innerHTML = "Medium";
+    passwordError.classList.add("hidden");
+  } else if (passwordcheck2(target)) {
+    passwordStrengthText.innerHTML = "Weak";
+    passwordStrength.classList.remove(
+      "bg-green-400",
+      "bg-orange-400",
+      "bg-gray-700"
+    );
+    passwordStrength.classList.add("bg-red-400");
+    UserData.Password = target;
+    passwordError.classList.add("hidden");
+  } else if (passwordcheck3(target)) {
+    passwordStrengthText.innerHTML = "Weak";
+    passwordStrength.classList.remove(
+      "bg-green-400",
+      "bg-orange-400",
+      "bg-gray-700"
+    );
+    passwordStrength.classList.add("bg-red-400");
+    UserData.Password = target;
+    passwordError.classList.add("hidden");
+  } else if (passwordcheck(target)) {
+    passwordStrengthText.innerHTML = "Weak";
+    passwordStrength.classList.remove(
+      "bg-green-400",
+      "bg-orange-400",
+      "bg-gray-700"
+    );
+    passwordStrength.classList.add("bg-red-400");
+    UserData.Password = target;
+    passwordError.classList.add("hidden");
+  }
+  console.log(UserData);
+});
+
+confirmPassword.addEventListener("change", (e) => {
+  sure = e.target.value;
+  console.log(sure);
+  if (UserData.Password == "") {
+    confirmPasswordError.classList.remove("hidden");
+    confirmPasswordError.innerHTML = "Please Enter the Password first";
+  } else if (sure != UserData.Password) {
+    confirmPasswordError.classList.remove("hidden");
+    confirmPasswordError.innerHTML =
+      "Confirm Password doesn't Match with Password";
+  } else if (sure == UserData.Password) {
+    confirmPasswordError.classList.add("hidden");
+  }
+  // console.log(UserData);
+});
+
+terms.addEventListener("change", (e) => {
+  check = e.target.checked;
+  console.log(check);
+  if (!check) {
+    termsError.classList.remove("hidden");
+    termsError.innerHTML = "Please Confirm the Terms and Conditions";
+  } else {
+    termsError.classList.add("hidden");
+  }
+  // console.log(UserData);
+});
+
+// rbtn = false;
+resetBtn.addEventListener("click", (e) => {
+  // rbtn = !rbtn;
+  // console.log(rbtn);
+  // if (rbtn == true) {
+  registrationForm.reset();
+  Array.from(tag).forEach((errortag) => {
+    errortag.classList.add("hidden");
   });
-}
-function emailcheck() {
-  document.addEventListener("keydown", (k) => {
-    if (k.key === " ") {
-      k.preventDefault();
+  // rbtn == false
+  // }
+});
+
+validateBtn.addEventListener("click", (e) => {
+  const firsterror = document.querySelector(".errortag:not(.hidden)");
+  let haserror = false;
+  Array.from(tag).forEach((errortag) => {
+    const finderror = errortag.classList.contains("hidden");
+    if (finderror == false) {
+      haserror = true;
     }
-    email.addEventListener("change", (e) => {
-      Email = e.target.value;
-      Email.toLowerCase();
-      if (!emailregex(Email)) {
-        emailError.classList.remove("hidden");
-        emailError.innerHTML = "Invalid Email";
-      } else {
-        UserData.EmailAdress = Email;
-        emailError.classList.add("hidden");
-      }
-      console.log(UserData);
+  });
+  Array.from(blankcheck).forEach((blank) => {
+    if (blank.value.trim() == "") {
+      haserror = true;
+    }
+  });
+  if (haserror) {
+    alert("Please fill Form properly");
+    firsterror.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
     });
-  });
-}
-function phonecheck() {
-  phone.addEventListener("change", (e) => {
-    Phone = e.target.value;
-    if (!number(Phone)) {
-      phoneError.classList.remove("hidden");
-      phoneError.innerHTML = "Invalid Mobile Number";
-    } else {
-      // Phone.replace(/(\d{5})/ , '$1 ')
-      UserData.PhoneNumber = Phone;
-      phoneError.classList.add("hidden");
-    }
-    console.log(UserData);
-  });
-}
-function usercheck() {
-  username.addEventListener("change", (e) => {
-    User = e.target.value;
-    if (!userregex(User)) {
-      usernameError.classList.remove("hidden");
-      usernameError.innerHTML =
-        "Invalid Username Maximum 15 Characters and only (Uppercase, Lowercase, Number)";
-    } else {
-      UserData.UserName = User;
-      usernameError.classList.add("hidden");
-    }
-    console.log(UserData);
-  });
-}
-function passwordchecker() {
-  toggle.addEventListener("click", () => {
-    input.type === "password"
-      ? (input.type = "text")
-      : (input.type = "password");
-  });
-
-  input.addEventListener("input", (e) => {
-    target = e.target.value;
-    console.log(target);
-    if (target.length < 8) {
-      passwordError.classList.remove("hidden");
-      passwordError.innerHTML = "Password must be greater than 8";
-      passwordStrength.classList.remove(
-        "bg-green-400",
-        "bg-orange-400",
-        "bg-gray-700"
-      );
-      passwordStrength.classList.add("bg-red-400");
-    } else if (
-      passwordcheck(target) &&
-      passwordcheck2(target) &&
-      passwordcheck3(target)
-    ) {
-      passwordStrengthText.innerHTML = "Strong";
-      passwordStrength.classList.remove(
-        "bg-red-400",
-        "bg-orange-400",
-        "bg-gray-700"
-      );
-      passwordStrength.classList.add("bg-green-400");
-      passwordError.classList.add("hidden");
-    } else if (
-      (passwordcheck(target) && passwordcheck2(target)) ||
-      (passwordcheck2(target) && passwordcheck3(target)) ||
-      (passwordcheck(target) && passwordcheck3(target))
-    ) {
-      passwordStrength.classList.add("bg-orange-400");
-      passwordStrength.classList.remove(
-        "bg-green-400",
-        "bg-red-400",
-        "bg-gray-700"
-      );
-      passwordStrengthText.innerHTML = "Medium";
-      passwordError.classList.add("hidden");
-    } else if (passwordcheck2(target)) {
-      passwordStrengthText.innerHTML = "Weak";
-      passwordStrength.classList.remove(
-        "bg-green-400",
-        "bg-orange-400",
-        "bg-gray-700"
-      );
-      passwordStrength.classList.add("bg-red-400");
-      UserData.Password = target;
-      passwordError.classList.add("hidden");
-    } else if (passwordcheck3(target)) {
-      passwordStrengthText.innerHTML = "Weak";
-      passwordStrength.classList.remove(
-        "bg-green-400",
-        "bg-orange-400",
-        "bg-gray-700"
-      );
-      passwordStrength.classList.add("bg-red-400");
-      UserData.Password = target;
-      passwordError.classList.add("hidden");
-    } else if (passwordcheck(target)) {
-      passwordStrengthText.innerHTML = "Weak";
-      passwordStrength.classList.remove(
-        "bg-green-400",
-        "bg-orange-400",
-        "bg-gray-700"
-      );
-      passwordStrength.classList.add("bg-red-400");
-      UserData.Password = target;
-      passwordError.classList.add("hidden");
-    }
-    console.log(UserData);
-  });
-}
-
-function confirm() {
-  confirmPassword.addEventListener("change", (e) => {
-    sure = e.target.value;
-    console.log(sure);
-    if (UserData.Password == "") {
-      confirmPasswordError.classList.remove("hidden");
-      confirmPasswordError.innerHTML = "Please Enter the Password first";
-    } else if (sure != UserData.Password) {
-      confirmPasswordError.classList.remove("hidden");
-      confirmPasswordError.innerHTML =
-        "Confirm Password doesn't Match with Password";
-    } else if (sure == UserData.Password) {
-      confirmPasswordError.classList.add("hidden");
-    }
-    // console.log(UserData);
-  });
-}
-function checkbox() {
-  terms.addEventListener("change", (e) => {
-    check = e.target.checked;
-    console.log(check);
-    if (!check) {
-      termsError.classList.remove("hidden");
-      termsError.innerHTML = "Please Confirm the Terms and Conditions";
-    } else {
-      termsError.classList.add("hidden");
-    }
-    // console.log(UserData);
-  });
-}
-  btn = false;
-  resetBtn.addEventListener("click", (e) => {
-    btn = !btn;
-    console.log(btn);
-    if (btn == true) {
-      registrationForm.reset();
-      Array.from(tag).forEach((errortag) => {
-        errortag.classList.add("hidden");
-      });
-    }
-    console.log(UserData);
-  });
+    sibling = firsterror.previousElementSibling
+    input = sibling.querySelector('input')
+    input.value = ""
+    input.focus()
+    // registrationForm.reset();
+    // Array.from(tag).forEach((errortag) => {
+    //   errortag.classList.add("hidden");
+    // });
+  } else {
+    json = JSON.stringify(UserData);
+    validateBtn.innerHTML = "Validating Form....";
+    setTimeout(() => {
+      set = localStorage.setItem("Userdata", json);
+      get = localStorage.getItem("Userdata");
+      if (get == json) {
+        alert("Your form has been Submitted");
+        validateBtn.innerHTML = "Validate Form";
+      } else {
+        alert("There's an Error please try again");
+        validateBtn.innerHTML = "Validate Form";
+      }
+    }, 3000);
+  }
+});
